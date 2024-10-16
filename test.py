499882096ffa4cdc6fbf93d44a126b6e5a794526
skander <skander.tlili@enstab.ucar.tn>
2024-10-16 11:02:18 +0100

    this is the first commit in the related branch

---
+++ test.py
@@ -0,0 +1,31 @@
+liste_1 = [1,5,6,4,3]
+
+liste_2 = [1,2,5,1,3]
+
+l_1 = len(liste_1)
+l_2 = len(liste_2)
+
+s_1 = 0
+s_2 = 0
+
+
+for i in range(l_1): # range(l_1) = [0,1,2,3,4]    
+    
+    s_1 = s_1 + liste_1[i]
+    s_2 = s_2 + liste_2[i]
+
+if s_1 > s_2 : 
+    print("Joueur 1 gagnant")
+elif  s_1 < s_2 : 
+    print("Joueur 2 gagnant")
+elif s_1 == s_2: 
+    print("le match est nul")
+
+
+print(s_1)
+print(s_2)
+    
+
+
+    
+
